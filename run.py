a21dfc034a97b2d3e2a76f28a2222e1d6f47d025
Grzegorz Czaplik <faflik@onet.pl>
2022-02-27 09:34:36 +0100

    add max_plants and min-balance

--- run.py
+++ run.py
@@ -5,18 +5,23 @@ import dotenv, os
 
 dotenv.load_dotenv('.env')
 
-
-how_many_plants = 1
+HOW_MANY_PLANTS = 1
+MAX_PLANTS = 1000
+MIN_BALANCE = 0.01
 
 # print(dir(contract.functions.plantSeeds))
 bsc = "https://bsc-dataseed.binance.org/"
 web3 = Web3(Web3.HTTPProvider(bsc))
 
 address = web3.toChecksumAddress(os.environ['ADDRESS'])
+balance = web3.eth.getBalance(os.environ['ADDRESS'])
+balance = web3.fromWei(balance, 'ether')
 
 contract_adres = web3.toChecksumAddress("0x685bfdd3c2937744c13d7de0821c83191e3027ff")
 contract = web3.eth.contract(address=contract_adres, abi=abi.ABI)
  
+plant_growing = contract.functions.hatcheryPlants(address).call()
+
 def ready_plant():
     readyplant = contract.functions.getUserSeeds(address).call()
     readyplant = math.floor(readyplant/2592000)
@@ -33,15 +38,24 @@ def send_transaction():
 
     signed_tx = web3.eth.account.sign_transaction(tx, private_key=os.environ['KEY'])
     txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-       
-    plant_growing = contract.functions.hatcheryPlants(address).call()
+
     with open('log.txt','a') as file:
-        file.write(f"Before plant: {plant_growing} TX: {web3.toHex(txn)} After plant: {plant_growing +1}\n")
+        file.write(f"Plant: {plant_growing} TX: {web3.toHex(txn)}\n")
 
 
 def main():
-    if ready_plant() >= how_many_plants:
+    if balance < MIN_BALANCE:
+        print('za malo srodkow')
+        exit()
+        
+    if plant_growing >= MAX_PLANTS:
+        print('koniec')
+        exit()
+        
+    if ready_plant() >= HOW_MANY_PLANTS:
         send_transaction()
+    else:
+        print('za malo')
 
 
 if __name__ == "__main__":
