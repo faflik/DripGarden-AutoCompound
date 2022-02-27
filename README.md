b815158691d9cbb78a8593f998990187a3f894b3
Grzegorz Czaplik <faflik@onet.pl>
2022-02-27 23:33:11 +0100

    fix working directory

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Buy some VPS to run script 24 hours per day, then follow steps below
    - `*/2 * * * * python3 /PATH_TO_FILE/run.py`
 
 5. Configure:
+   - PATH_TO_FILE = '/home/USER/DripGarden-AutoCompound/' # absolute path to working directory
    - HOW_MANY_PLANTS = 1 # if 1 compound each plant, e.g 5 - wait for 5 plant to compound
    - MAX_PLANTS = 2000   # compound to this value and stop
    - MIN_BALANCE = 0.01  # minimum account BNB balance below which stop compound
--- run.py
+++ run.py
@@ -3,20 +3,23 @@ import math
 import abi
 import dotenv, os, sys
    
-
+# absolute path to working directory e.g /home/USER/DripGarden-AutoCompound/   
+PATH_TO_FILE = ''
 HOW_MANY_PLANTS = 1 # if 1 compound each plant, e.g 5 - wait for 5 plant to compound
 MAX_PLANTS = 2000   # compound to this value and stop
 MIN_BALANCE = 0.01  # minimum account BNB balance below which stop compound
 
-dotenv.load_dotenv(os.getcwd()+'/.env')
+dotenv.load_dotenv(PATH_TO_FILE+'.env')
 
 bsc = "https://bsc-dataseed.binance.org/"
 web3 = Web3(Web3.HTTPProvider(bsc))
-
-address = web3.toChecksumAddress(os.environ['ADDRESS'])
-balance = web3.eth.getBalance(os.environ['ADDRESS'])
-balance = web3.fromWei(balance, 'ether')
-
+try:
+    address = web3.toChecksumAddress(os.environ['ADDRESS'])
+    balance = web3.eth.getBalance(os.environ['ADDRESS'])
+    balance = web3.fromWei(balance, 'ether')
+except:
+    print('Set correct PATH_TO_FILE value')
+    sys.exit()
 contract_adres = web3.toChecksumAddress("0x685bfdd3c2937744c13d7de0821c83191e3027ff")
 contract = web3.eth.contract(address=contract_adres, abi=abi.ABI)
  
@@ -39,7 +42,7 @@ def send_transaction():
     signed_tx = web3.eth.account.sign_transaction(tx, private_key=os.environ['KEY'])
     txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
-    with open(os.getcwd()+'log.txt','a') as file:
+    with open(PATH_TO_FILE+'log.txt','a') as file:
         file.write(f"Plant: {plant_growing} TX: {web3.toHex(txn)}\n")
 
 
