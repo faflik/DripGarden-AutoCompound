7deb5bb753a311899690bfb7c34d804014926503
Grzegorz Czaplik <faflik@onet.pl>
2022-03-06 18:05:48 +0100

    changed dir_path

--- run.py
+++ run.py
@@ -3,13 +3,14 @@ import math
 import abi
 import dotenv, os, sys
    
-# absolute path to working directory e.g /home/USER/DripGarden-AutoCompound/   
-PATH_TO_FILE = ''
+# path to working directory  
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 HOW_MANY_PLANTS = 1 # if 1 compound each plant, e.g 5 - wait for 5 plant to compound
 MAX_PLANTS = 2000   # compound to this value and stop
 MIN_BALANCE = 0.01  # minimum account BNB balance below which stop compound
 
-dotenv.load_dotenv(PATH_TO_FILE+'.env')
+dotenv.load_dotenv(dir_path + '/.env')
 
 bsc = "https://bsc-dataseed.binance.org/"
 web3 = Web3(Web3.HTTPProvider(bsc))
@@ -18,7 +19,7 @@ try:
     balance = web3.eth.getBalance(os.environ['ADDRESS'])
     balance = web3.fromWei(balance, 'ether')
 except:
-    print('Set correct PATH_TO_FILE value')
+    print('Wrong dir_path')
     sys.exit()
 contract_adres = web3.toChecksumAddress("0x685bfdd3c2937744c13d7de0821c83191e3027ff")
 contract = web3.eth.contract(address=contract_adres, abi=abi.ABI)
@@ -42,7 +43,7 @@ def send_transaction():
     signed_tx = web3.eth.account.sign_transaction(tx, private_key=os.environ['KEY'])
     txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
-    with open(PATH_TO_FILE+'log.txt','a') as file:
+    with open(dir_path + '/log.txt','a') as file:
         file.write(f"Plant: {plant_growing} TX: {web3.toHex(txn)}\n")
 
 
@@ -61,4 +62,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
