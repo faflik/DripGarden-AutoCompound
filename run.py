7aa91c3225136d3405f1688006645e8c03821379
Grzegorz Czaplik <faflik@onet.pl>
2022-04-02 19:23:46 +0200

    customize log system

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from web3 import Web3
 import math
 import abi
-import dotenv, os, sys
+import dotenv, os, sys, time
    
 # path to working directory  
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 HOW_MANY_PLANTS = 1 # if 1 compound each plant, e.g 5 - wait for 5 plant to compound
-MAX_PLANTS = 2000   # compound to this value and stop
+MAX_PLANTS = 20000   # compound to this value and stop
 MIN_BALANCE = 0.02  # minimum account BNB balance below which stop compound
 LP_DAY = 3.500      # Limit Lp per Day. Below plant seeds, above sell seeds
 
@@ -20,7 +20,8 @@ try:
     balance = web3.eth.getBalance(os.environ['ADDRESS'])
     balance = web3.fromWei(balance, 'ether')
 except:
-    print('Wrong dir_path')
+    with open(dir_path + '/log.txt','a') as file:
+        file.write("Wrong dirpath\n")
     sys.exit()
 contract_adres = web3.toChecksumAddress("0x685bfdd3c2937744c13d7de0821c83191e3027ff")
 contract = web3.eth.contract(address=contract_adres, abi=abi.ABI)
@@ -52,7 +53,7 @@ def plantSeed():
     txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     with open(dir_path + '/log.txt','a') as file:
-        file.write(f"Plant TX: {web3.toHex(txn)}\n")
+        file.write(f"{time.strftime(format('%d.%m %H:%M'))} Plant {ready_plant()} - {lpDay()} lpDay\n")
 
 def sellSeed():
     nonce = web3.eth.get_transaction_count(address)
@@ -67,13 +68,18 @@ def sellSeed():
     txn = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     with open(dir_path + '/log.txt','a') as file:
-        file.write(f"Sell TX: {web3.toHex(txn)}\n")
+        file.write(f"{time.strftime(format('%d.%m %H:%M'))} Sell {ready_plant()} - {lpDay()} lpDay\n")
+
 
 def main():
     if balance < MIN_BALANCE:
+        with open(dir_path + '/log.txt','a') as file:
+            file.write(f"{time.strftime(format('%d.%m %H:%M'))} Balance too low - {lpDay()} lpDay\n")
         sys.exit()
 
     if plant_growing >= MAX_PLANTS:
+        with open(dir_path + '/log.txt','a') as file:
+            file.write(f"{time.strftime(format('%d.%m %H:%M'))} Plants limit reached - {lpDay()} lpDay\n")
         sys.exit()
 
     if ready_plant() >= HOW_MANY_PLANTS:
