03c5626e26cc054fe691d42bea085bba05c926b8
Grzegorz Czaplik <faflik@onet.pl>
2022-04-15 17:20:49 +0200

    add config.py and round.txt

---
+++ config.py
@@ -0,0 +1,7 @@
+HOW_MANY_PLANTS = 10 # if 1 compound or sell each plant, e.g 5 - wait for 5 plant to compound or sell
+MIN_BALANCE = 0.02  # minimum account BNB balance below which stop compound
+LP_DAY = 3.500      # Limit Lp per Day. Below plant seeds, above sell seeds
+
+PERIOD = 0          # 0 - plant every HOW_MANY_PLANTS
+                    # 1 - harvest every HOW_MANY_PLANTS
+                    # e.g. 3 - harvest every third HOW_MANY_PLANTS, first and second plant
